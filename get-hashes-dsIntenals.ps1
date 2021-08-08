6c9fdbc98d0f623f316db542f43777bb3e4ae935
Daniel Card <dan.card@xservus.com>
2021-08-08 13:38:54 +0100

    Update get-hashes-dsIntenals.ps1

--- get-hashes-dsIntenals.ps1
+++ get-hashes-dsIntenals.ps1
@@ -12,3 +12,8 @@ $ntdsaudit | Format-Custom -View HashcatNT
 $ntdsaudit  | Format-Custom -View HashcatNTHistory
 $ntdsaudit  | Format-Custom -View JohnNT
 $ntdsaudit  | Format-Custom -View JohnNTHistory
+
+# to dump these try 
+$ntdsaudit  | Format-Custom -View HashcatNTHistory | Out-File C:\users\mRr3b00t\Downloads\hashcat-6.2.2\hashhashhash.txt -Encoding utf8
+
+#the encoding matters ;) - mRr3b00t
